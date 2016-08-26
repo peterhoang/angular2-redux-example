b3609397bfe444fe90dd48fb590e6107c1ff907f
SOTI\phoang <peter.hoang@soti.net>
2016-08-26 10:12:16 -0400

    Added pagination

--- src/components/thumbnail-grid/thumbnail-grid.component.ts
+++ src/components/thumbnail-grid/thumbnail-grid.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
 import { select } from 'ng2-redux';
-import { InfiniteScroll } from 'angular2-infinite-scroll';
 import { MarvelActions } from '../../actions/marvel.actions';
 import { MyAppSettings } from '../../app/my-app.settings';
 import { Map } from 'immutable';
@@ -15,21 +14,20 @@ import 'rxjs/add/operator/scan';
 @Component({
   pipes: [AsyncPipe],
   selector: 'thumbnail-grid',
-  template: require('./thumbnail-grid.component.html'),
-  directives: [ InfiniteScroll ]
+  template: require('./thumbnail-grid.component.html')
 })
 export class ThumbnailGrid {
   @select(['marvel', 'data', 'results', 0, 'thumbnail', 'path']) path$: Observable<string>;
   @select(['marvel', 'data', 'results', 0, 'thumbnail', 'extension']) ext$: Observable<string>;
-  @select(['marvel', 'data', 'results']) results$: Observable<Array<Object>>;
   @select(['marvel', 'data']) data$: Observable<Array<Object>>;
   @select(['marvel', 'attributionText']) attText$: Observable<string>;
 
   private characters$: Observable<Array<Object>>;
   private offset: number;
+  private pageNumber: number;
+  private collectionSize: number;
 
   constructor(private actions: MarvelActions, private appSettings: MyAppSettings) {
-    this.offset = 0;
   }
 
   ngOnInit() {
@@ -48,18 +46,28 @@ export class ThumbnailGrid {
 
     this.characters$ = this.data$
       .map((data: any) => {
-        // Ugly solution for now: at the beginning of fresh app launch, the persistent store is deimmutify
+        // Ugly solution for now: at the beginning of fresh app launch, the persistent store is deimmutify (js object)
         // however, during app session, the reducer returns the store's state as immutable objects
         if (Map.isMap(data)) {
           data = data.toJS();
         }
 
-        let character = data.results;        
+        this.collectionSize = data.total;
+
+        // determine current page
+        this.pageNumber = (data.offset / data.limit) + 1;
+        this.offset = data.offset;
+
+        let character = data.results;
         character.forEach(char => {
           char.resolvedImage = (char.thumbnail.path + '/standard_amazing.' + char.thumbnail.extension);
         });
         return character;
       });
+  }
+
+  onPageChange(pageNumber: number) {
+    this.actions.getCharacters({ offset: this.appSettings.ITEMS_PER_PAGE * (pageNumber - 1) });
   }
 
   onScrollDown() {
