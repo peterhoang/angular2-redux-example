0361202999e31783391d7a7cd705b83349f9e6c6
Peter <popsiclepete@gmail.com>
2016-08-26 22:20:14 -0400

    MarvelReduce unit tests

--- src/store/marvel/marvel.reducer.test.ts
+++ src/store/marvel/marvel.reducer.test.ts
@@ -11,7 +11,7 @@ describe('Marvel Reducer', () => {
   });
 
   it('should have an immutable initial state', () => {
-    expect(Iterable.isIterable(initState).toBe(true));
+    expect(Iterable.isIterable(initState)).toBe(true);
   });
 
   it('should set loading to be true on MARVEL_GET_CHARACTERS', () => {
@@ -19,5 +19,42 @@ describe('Marvel Reducer', () => {
       initState,
       { type: MarvelActions.GET_CHARACTERS });
     expect(nextState.get('isLoading')).toBeTruthy;
+  });
+
+  it('should set a status OK on MARVEL_GET_CHARACTERS_SUCCESS', () => {
+    const nextState = marvelReducer(
+      initState,
+      { type: MarvelActions.GET_CHARACTERS_SUCCESS,
+        payload: {
+          code: 200,
+          status: "Ok",
+          copyright: null,
+          attributionText: null,
+          attributionHTML: null,
+          data: null
+        }
+      });
+    expect(nextState.get('isLoading')).toBeFalsy;
+    expect(nextState.get('hasError')).toBeFalsy;
+    expect(nextState.get('code')).toEqual(200);
+    expect(nextState.get('status')).toEqual("Ok");
+    expect(nextState.get('copyright')).toEqual(null);
+    expect(nextState.get('attributionText')).toEqual(null);
+    expect(nextState.get('attributionHTML')).toEqual(null);
+    expect(nextState.get('characters')).toEqual(null);
+  });
+
+  it('should flag an error on MARVEL_GENERIC_ERROR', () => {
+    const nextState = marvelReducer(
+      initState,
+      {
+        type: MarvelActions.MARVEL_GENERIC_ERROR,
+        payload: {
+          code: null,
+          status: null
+        }
+      });
+    expect(nextState.get('isLoading')).toBeFalsy;
+    expect(nextState.get('hasError')).toBeTruthy;
   });
 });
